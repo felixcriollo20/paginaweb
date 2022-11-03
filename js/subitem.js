39624ecc0e49debee64d7cbc1f9c0ab256fb100f
felixcriollo <criolloyefry@gmail.com>
2022-11-03 11:08:53 -0500

    modificaciones

--- js/subitem.js
+++ js/subitem.js
@@ -1,2 +1,18 @@
-<script src="js/script.js"> 
-</script>
+let inicio= document . getElementById ("inicio");
+inicio.addEventListener("click", function() {
+    alert("este es el menu de incio")
+});
+
+let nosotros= document . getElementById ("nosotros");
+nosotros.addEventListener("click", function() {
+    alert("somos un gran equipo,te ayudamos")
+});
+
+let conctatenos= document . getElementById ("conctatenos");
+conctatenos.addEventListener("click", function() {
+    alert("tirese el correo o llamenos a la linea 01 80000 202011")
+});
+
+let ayuda= document . getElementById ("ayuda");
+    ayuda.addEventListener("click", function(){
+    alert("salvece quien pueda")});
