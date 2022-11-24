8069fce0721f1daec5adc4fff5071b781ece7368
felixcriollo <criolloyefry@gmail.com>
2022-11-24 10:09:47 -0500

    modificaciones

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-let botonmenu = " "
+let botonmenu = document.getElementById ("bolo");
+let menu = document.getElementById ('nava2');
+
+botonmenu.addEventListener("click", function(){
+    if (menu.classList.contains("pull")){
+        menu.classList.add ("pull-visible");
+        menu.classList.remove ("pull");
+    } else {
+        menu.classList.remove("pull-visible");
+        menu.classList.add ("pull");
+    }
+});
